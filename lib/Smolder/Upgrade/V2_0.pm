50964ba39547bd11508e2e010b4cbde6db7af69d
Mike Cartmell <mike.cartmell@spareroom.co.uk>
2013-01-11 15:39:18 +0000

    forgot upgrade modules

---
+++ lib/Smolder/Upgrade/V2_0.pm
@@ -0,0 +1,9 @@
+package Smolder::Upgrade::V2_0;
+use strict;
+use warnings;
+use base 'Smolder::Upgrade';
+
+sub pre_db_upgrade  { }
+sub post_db_upgrade { }
+
+1;
---
+++ sql/upgrade/V2_0.sql
@@ -0,0 +1,1 @@
+ALTER TABLE project ADD COLUMN vcs_rev_url TEXT DEFAULT '';
