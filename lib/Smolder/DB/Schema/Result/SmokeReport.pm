e0c32db9964415e61742223c22a316c4a133d4fc
Sebastien Aperghis-Tramoni <sebastien@aperghis.net>
2014-03-16 15:32:49 +0100

    remove useless parens

--- lib/Smolder/DB/Schema/Result/SmokeReport.pm
+++ lib/Smolder/DB/Schema/Result/SmokeReport.pm
@@ -379,16 +379,17 @@ sub _send_emails {
     my ($self, $results) = @_;
 
     # determine the appropriate status for the e-mail
-    my $status = ( $self->failed() ? "FAILURE" : "SUCCESS" );
-       $status = "WARNING" if( $self->skip >  0 ); 
-       $status = "ABORTED" if( $self->skip == $self->pass );
+    my $status = $self->failed() ? "FAILURE" : "SUCCESS";
+       $status = "WARNING" if $self->skip >  0; 
+       $status = "ABORTED" if $self->skip == $self->pass;
+
     # setup some stuff for the emails that we only need to do once
-    my $subject = sprintf("Smolder - [%s] %i files ( %i/%i ) : %s",
+    my $subject = sprintf "Smolder - [%s] %i files ( %i/%i ) : %s",
                           $self->project->name,
                           $self->test_files,
                           int( $self->pass - $self->skip ),
                           $self->total,
-                          $status );
+                          $status;
 
     my $matrix = Smolder::TAPHTMLMatrix->new(
         smoke_report => $self,
