072a36926f0558c1e1f21469acbb59a9132cb5c4
Sebastien Aperghis-Tramoni <sebastien@aperghis.net>
2014-03-16 13:44:49 +0100

    remove some old code

--- lib/Smolder/DB/Schema/Result/SmokeReport.pm
+++ lib/Smolder/DB/Schema/Result/SmokeReport.pm
@@ -379,9 +379,6 @@ sub _send_emails {
     my ($self, $results) = @_;
 
     # setup some stuff for the emails that we only need to do once
-#    my $subject =
-#      "[" . $self->project->name . "] new " . ($self->failed ? "failed " : '') . "Smolder report";
-
     my $subject = sprintf("Smolder - [%s] passed %i/%i tests: %s",
                           $self->project->name(),
                           $self->pass(),
